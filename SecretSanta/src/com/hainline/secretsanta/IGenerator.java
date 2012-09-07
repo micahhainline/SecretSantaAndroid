877e97fceba99b06c8cae39dfbcc3561eb97f807
Ruben Quintero <ruben.quintero@asolutions.com>
2012-09-07 10:13:43 -0500

    Added instruction comment.

--- SecretSanta/src/com/hainline/secretsanta/IGenerator.java
+++ SecretSanta/src/com/hainline/secretsanta/IGenerator.java
@@ -4,5 +4,6 @@ import java.util.Collection;
 import java.util.List;
 
 public interface IGenerator {
+	// Assignments should be returned in alphabetical order by giver's name
 	List<Assignment> createAssignmentsForPeople(Collection<Person> people);
 }
